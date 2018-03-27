12909bca03d00d2642ad77b2aca1a8ec6abbae7e
awestover <alek.westover@gmail.com>
2018-03-27 16:46:06 -0400

    added

--- psKILL.py
+++ psKILL.py
@@ -1,13 +1,17 @@
 import psutil
+loud=False
+
 for proc in psutil.process_iter():
 	try:
 		pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
 	except psutil.NoSuchProcess:
 		pass
 	else:
-		print(pinfo)
+		if loud:
+			print(pinfo)
 		if 'python' in pinfo['name']:
-			print(pinfo['pid'])
+			if loud:
+				print(pinfo['pid'])
 			proc.kill()
 
 
