e522f3d90cc15f7f2cf528c22b528b3911b6f1a3
Alek Westover <alek.westover@gmail.com>
2020-07-27 22:05:44 -0400

    reorganize

--- dotFiles/copy.sh
+++ dotFiles/copy.sh
@@ -1,9 +1,9 @@
 cp ~/.oh-my-zsh/custom/aliases.zsh .
 cp -r ~/.zshrc .
-cp -r ~/.config/nvim/autoload .
-cp ~/.config/nvim/init.vim .
-cp ~/.config/nvim/plugin-config.vimrc .
-cp -r ~/.config/nvim/spell .
-cp -r ~/.config/nvim/colors .
-cp -r ~/.config/nvim/save_temp_changes .
-cp ~/.config/nvim/vimPlug.vimrc .
+cp -r ~/.config/nvim/autoload nvim
+cp ~/.config/nvim/init.vim nvim
+cp ~/.config/nvim/plugin-config.vimrc nvim
+cp -r ~/.config/nvim/spell nvim
+cp -r ~/.config/nvim/colors nvim
+cp -r ~/.config/nvim/save_temp_changes nvim
+cp ~/.config/nvim/vimPlug.vimrc nvim
