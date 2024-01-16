cff6e0d584debe160045c1cc5b5311a5bdeaeaf6
Alek Westover <alek.westover@gmail.com>
2024-01-15 22:09:04 -0500

    fix

--- latexDocs/svg_to_png.py
+++ latexDocs/svg_to_png.py
@@ -2,26 +2,22 @@ import sys
 trigger_file = sys.argv[1]
 
 relevant_images = []
-
 with open(trigger_file, 'r') as f:
     for line in f.readlines():
-        if "images/" in line and ".svg" in line:
-            ![ink_img011](images/ink_img011.png)
-            start_pos = text.find('(images/') + 1
-            end_pos = text.find('.png)')
+        if "images/" in line and ".png" in line:
+            #  ![ink_img011](images/ink_img011.png)
+            start_pos = line.find('(images/') + len("(images/")
+            end_pos = line.find('.png)')
 
             if start_pos == 0 or end_pos == -1:
-                return continue
+                continue
             else:
-                relevant_images.append(line[start_pos:end_pos]+".svg")
+                relevant_images.append(line[start_pos:end_pos])
 
 import os
 os.chdir("images")
 
-#  for fname in os.listdir():
-  #  if ".svg" in fname:
 for fname in relevant_images:
-    newfname = fname.replace(".svg", ".png")
-    os.system(f"inkscape {fname} -o {newfname}")
+    os.system(f"inkscape {fname}.svg -o {fname}.png")
 
 
